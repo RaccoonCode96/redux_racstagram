8f9d1ccf89a2d765f523b429219efd3ea0f2cc90
RaccoonCode96 <waawoo13@gmail.com>
2021-08-04 16:38:13 +0900

    navigation 아이콘 현재 path 반영 표시 기능 구현 (navigation 아이콘 클릭시 현재 path이면  Outlined에서 Filled 아이콘으로 변경함)

--- src/components/common/Navigation.jsx
+++ src/components/common/Navigation.jsx
@@ -1,18 +1,20 @@
-import { faPlusSquare } from '@fortawesome/free-regular-svg-icons';
-import { faHome } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './Navigation.scss';
 import SignOutContainer from '../SignOut/SignOutContainer';
 import Menu from './Menu';
+import HomeOutlinedIcon from '@material-ui/icons/HomeOutlined';
+import HomeIcon from '@material-ui/icons/Home';
+import AddBoxIcon from '@material-ui/icons/AddBox';
+import AddBoxOutlinedIcon from '@material-ui/icons/AddBoxOutlined';
 
 const Navigation = () => {
 	const [isOn, setisOn] = useState(false);
 	const userPhotoUrl = useSelector(
 		(state) => state.users.currentUserInfo.userPhotoUrl
 	);
+	const { pathname } = useLocation();
 	const toggle = (event) => {
 		setisOn(!isOn);
 	};
@@ -25,20 +27,20 @@ const Navigation = () => {
 					<ul className="items">
 						<li className="item">
 							<Link to="/">
-								<FontAwesomeIcon
-									className="icon_home"
-									icon={faHome}
-									size={'2x'}
-								/>
+								{pathname === '/' ? (
+									<HomeIcon className="icon_home" />
+								) : (
+									<HomeOutlinedIcon className="icon_home" />
+								)}
 							</Link>
 						</li>
 						<li className="item">
 							<Link to="/write">
-								<FontAwesomeIcon
-									className="nav_NoPortrait"
-									icon={faPlusSquare}
-									size="2x"
-								/>
+								{pathname === '/write' ? (
+									<AddBoxIcon className="icon_write" />
+								) : (
+									<AddBoxOutlinedIcon className="icon_write" />
+								)}
 							</Link>
 						</li>
 						<li className="item">
@@ -47,6 +49,9 @@ const Navigation = () => {
 								onClick={toggle}
 								alt="user_image"
 								className="nav_profile_image"
+								style={
+									pathname === '/profile' ? { border: '' } : { border: 'none' }
+								}
 							/>
 							<Menu toggle={toggle} isOn={isOn} location={'nav_menu_location'}>
 								<Link to="/profile" className="menu_item">
