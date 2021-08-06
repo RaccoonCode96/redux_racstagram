2932d6e5c2d3d289acc6221f8ac5a347acace08e
RaccoonCode96 <waawoo13@gmail.com>
2021-08-06 13:24:12 +0900

    material ui alert을 활용하여 error 표시 스타일 구현

--- src/components/common/showError.jsx
+++ src/components/common/showError.jsx
@@ -1,9 +1,23 @@
+import { Alert } from '@material-ui/lab';
 import { useSelector } from 'react-redux';
+import './ShowError.scss';
 
 const ShowError = () => {
 	const errorSelector = useSelector((state) => state.auth.errorSelector);
-
-	return <h4 className="auth_error">{errorSelector}</h4>;
+	return (
+		<div className="show_error">
+			{errorSelector ? (
+				<Alert severity="warning" className="message">
+					{errorSelector === 'auth/user-not-found'
+						? '가입하지 않은 아이디이거나, 잘못된 비밀번호입니다.'
+						: errorSelector}
+				</Alert>
+			) : (
+				<></>
+			)}
+		</div>
+	);
+	// <h4 className="auth_error">{errorSelector}</h4>;
 };
 
 export default ShowError;
