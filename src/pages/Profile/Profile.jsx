2ff1243f14df8a9f2ab9a4aadeb67c5f20b32bbe
RaccoonCode96 <waawoo13@gmail.com>
2021-08-06 13:43:47 +0900

    '/profile', '/user/:userName' Route의 페이지 공유로 인한 기존 코드 삭제

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,27 +1,13 @@
 import UserProfileContainer from '../../components/UserProfile/UserProfileContainer';
-import { useCallback } from 'react';
-import { useDispatch } from 'react-redux';
-import { getCurrentUserInfoThunk } from '../../redux/modules/users';
-import { getCurrentUserPostsThunk } from '../../redux/modules/post';
+
 import Navigation from '../../components/common/Navigation';
 const Profile = () => {
-	const dispatch = useDispatch();
-
-	const getInfoPosts = useCallback(async () => {
-		dispatch(getCurrentUserInfoThunk());
-		dispatch(getCurrentUserPostsThunk());
-	}, [dispatch]);
-
 	return (
 		<>
 			<Navigation />
 			<div className="page">
 				<div className="inner">
-					<UserProfileContainer
-						getInfoPosts={getInfoPosts}
-						postsType={'currentUserPosts'}
-						infoType={'currentUserInfo'}
-					/>
+					<UserProfileContainer />
 				</div>
 			</div>
 			<div className="modal_root"></div>
