21578f8ed3b1af2257e720a9fc9acfec2b0c8942
RaccoonCode96 <waawoo13@gmail.com>
2021-08-07 12:09:23 +0900

    클래스명 구체화 (form -> auth_form)

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -17,88 +17,86 @@ const AuthForm = ({
 	const { email, password, displayName } = inputs;
 
 	return (
-		<>
-			<form onSubmit={onSubmit} className="form">
-				<TextField
-					variant="outlined"
-					name="email"
-					type="email"
-					value={email}
-					onChange={onChange}
-					className="input"
-					label="Email"
-					size="small"
-				/>
-				<TextField
-					className="input"
-					variant="outlined"
-					name="password"
-					type="password"
-					label="Password"
-					size="small"
-					value={password}
-					onChange={onChange}
-				/>
-				{newAccount ? (
-					<>
-						<div className="container">
-							<TextField
-								className="displayName_input"
-								variant="outlined"
-								name="displayName"
-								label="User Name"
-								type="text"
-								size="small"
-								value={displayName}
-								onChange={onChange}
-							/>
-							<Button
-								className="displayName_btn"
-								variant="outlined"
-								color="primary"
-								size="small"
-								onClick={(event) => {
-									event.preventDefault();
-									check(displayName);
-								}}
-							>
-								중복 검사
-							</Button>
-						</div>
-						{checkDisplayName ? (
-							<CircularProgress />
-						) : !exist[1] || displayName !== exist[1] ? (
-							<Alert className="check_message" severity="warning">
-								중복 확인이 필요합니다.
-							</Alert>
-						) : exist[0] ? (
-							<Alert className="check_message" severity="error">
-								{exist[1]}는 이미 존재하는 이름 입니다!
-							</Alert>
-						) : (
-							<Alert className="check_message" severity="success">
-								OK!
-							</Alert>
-						)}
-					</>
-				) : (
-					<></>
-				)}
-				{emailSignIn || emailSignUp || socialSignIn ? (
-					<CircularProgress />
-				) : (
-					<Button
-						variant="contained"
-						color="primary"
-						className="auth_btn"
-						type="submit"
-						disableElevation
-					>
-						{newAccount ? '가입하기' : '로그인'}
-					</Button>
-				)}
-			</form>
-		</>
+		<form onSubmit={onSubmit} className="auth_form">
+			<TextField
+				variant="outlined"
+				name="email"
+				type="email"
+				value={email}
+				onChange={onChange}
+				className="input"
+				label="Email"
+				size="small"
+			/>
+			<TextField
+				className="input"
+				variant="outlined"
+				name="password"
+				type="password"
+				label="Password"
+				size="small"
+				value={password}
+				onChange={onChange}
+			/>
+			{newAccount ? (
+				<>
+					<div className="container">
+						<TextField
+							className="displayName_input"
+							variant="outlined"
+							name="displayName"
+							label="User Name"
+							type="text"
+							size="small"
+							value={displayName}
+							onChange={onChange}
+						/>
+						<Button
+							className="displayName_btn"
+							variant="outlined"
+							color="primary"
+							size="small"
+							onClick={(event) => {
+								event.preventDefault();
+								check(displayName);
+							}}
+						>
+							중복 검사
+						</Button>
+					</div>
+					{checkDisplayName ? (
+						<CircularProgress />
+					) : !exist[1] || displayName !== exist[1] ? (
+						<Alert className="check_message" severity="warning">
+							중복 확인이 필요합니다.
+						</Alert>
+					) : exist[0] ? (
+						<Alert className="check_message" severity="error">
+							{exist[1]}는 이미 존재하는 이름 입니다!
+						</Alert>
+					) : (
+						<Alert className="check_message" severity="success">
+							OK!
+						</Alert>
+					)}
+				</>
+			) : (
+				<></>
+			)}
+			{emailSignIn || emailSignUp || socialSignIn ? (
+				<CircularProgress />
+			) : (
+				<Button
+					variant="contained"
+					color="primary"
+					className="auth_btn"
+					type="submit"
+					disableElevation
+				>
+					{newAccount ? '가입하기' : '로그인'}
+				</Button>
+			)}
+		</form>
 	);
 };
 
