0abf4bfe4c814dde9d5fb00ad062c82d6996b6e2
RaccoonCode96 <waawoo13@gmail.com>
2021-08-07 11:19:49 +0900

    Auth Error Message 설정

--- src/components/common/showError.jsx
+++ src/components/common/showError.jsx
@@ -4,13 +4,21 @@ import './ShowError.scss';
 
 const ShowError = () => {
 	const errorSelector = useSelector((state) => state.auth.errorSelector);
+	const check = () => {
+		if (
+			errorSelector === 'auth/user-not-found' ||
+			errorSelector === 'auth/wrong-password'
+		) {
+			return '가입하지 않은 아이디이거나, 잘못된 비밀번호입니다.';
+		} else {
+			return errorSelector;
+		}
+	};
 	return (
 		<div className="show_error">
 			{errorSelector ? (
 				<Alert severity="warning" className="message">
-					{errorSelector === 'auth/user-not-found'
-						? '가입하지 않은 아이디이거나, 잘못된 비밀번호입니다.'
-						: errorSelector}
+					{check()}
 				</Alert>
 			) : (
 				<></>
