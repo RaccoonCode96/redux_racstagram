754b2cd19cb84b9986982def0b858bb967c6eb20
RaccoonCode96 <waawoo13@gmail.com>
2021-08-08 22:10:54 +0900

    main el의 children 중에서 선택된 post el의 위치로 스크롤 이동 구현

--- src/pages/Posts/Posts.jsx
+++ src/pages/Posts/Posts.jsx
@@ -1,18 +1,34 @@
+import { useEffect, useRef } from 'react';
 import { useLocation } from 'react-router-dom';
 import Navigation from '../../components/common/Navigation';
 import PostContainer from '../../components/Post/PostContainer';
 
 const Posts = () => {
 	const {
-		state: { posts },
+		state: { posts, postNum },
 	} = useLocation();
-
+	const targetRef = useRef();
+	const scrollToPost = (num) => {
+		if (!num) {
+			return;
+		}
+		const top = targetRef.current.children[num].offsetTop;
+		// window.scrollBy({
+		// 	top: top - 54,
+		// 	left: 0,
+		// 	behavior: 'smooth',
+		// });
+		window.scrollTo(0, top - 54);
+	};
+	useEffect(() => {
+		scrollToPost(postNum);
+	}, [postNum]);
 	return (
 		<>
 			<Navigation />
 			<div className="page">
 				<div className="inner">
-					<div className="main">
+					<div className="main" ref={targetRef}>
 						<PostContainer posts={posts} />
 					</div>
 					<div className="side">side</div>
