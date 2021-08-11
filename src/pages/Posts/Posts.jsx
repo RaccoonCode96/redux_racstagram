4c6be49372241259104746e1f9e28c70fdd17b53
RaccoonCode96 <waawoo13@gmail.com>
2021-08-11 21:35:41 +0900

    posts를 location으로 받오는 방식에서 redux state를 참조하도록 함(무한 스크롤의 일치를 위함)

--- src/pages/Posts/Posts.jsx
+++ src/pages/Posts/Posts.jsx
@@ -1,13 +1,20 @@
+import { useCallback } from 'react';
 import { useEffect, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useLocation } from 'react-router-dom';
 import Navigation from '../../components/common/Navigation';
+import Side from '../../components/common/Side';
 import PostContainer from '../../components/Post/PostContainer';
+import { resetGetMorePosts } from '../../redux/modules/post';
 
 const Posts = () => {
 	const {
-		state: { posts, postNum },
+		pathname,
+		state: { postNum },
 	} = useLocation();
+	const { currentUserPosts, userPosts } = useSelector((state) => state.post);
 	const targetRef = useRef();
+	const dispatch = useDispatch();
 	const scrollToPost = (num) => {
 		if (!num) {
 			return;
@@ -20,18 +27,29 @@ const Posts = () => {
 		// });
 		window.scrollTo(0, top - 54);
 	};
+	const selectPosts = useCallback(() => {
+		if (pathname.includes('/user/')) {
+			return userPosts;
+		} else if (pathname.includes('/profile/posts')) {
+			return currentUserPosts;
+		}
+	}, [pathname, userPosts, currentUserPosts]);
+
 	useEffect(() => {
 		scrollToPost(postNum);
-	}, [postNum]);
+		return () => {
+			dispatch(resetGetMorePosts());
+		};
+	}, [postNum, dispatch]);
 	return (
 		<>
 			<Navigation />
 			<div className="page">
 				<div className="inner">
 					<div className="main" ref={targetRef}>
-						<PostContainer posts={posts} />
+						<PostContainer posts={selectPosts()} />
 					</div>
-					<div className="side">side</div>
+					<Side />
 				</div>
 			</div>
 			<div className="modal_root"></div>
