13de7680b2118ac5ce2bd832925860d72c48ef3f
RaccoonCode96 <waawoo13@gmail.com>
2021-08-11 21:38:57 +0900

    다른 페이지 request 낭비 방지를 위해서 최대한으로 postreset을 제거함

--- src/pages/Write/Write.jsx
+++ src/pages/Write/Write.jsx
@@ -1,16 +1,7 @@
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
 import Navigation from '../../components/common/Navigation';
 import PostFormContainer from '../../components/PostForm/PostFormContainer';
-import { resetPost } from '../../redux/modules/post';
 
 const Write = () => {
-	const dispatch = useDispatch();
-	useEffect(() => {
-		return () => {
-			dispatch(resetPost());
-		};
-	}, [dispatch]);
 	return (
 		<>
 			<Navigation />
