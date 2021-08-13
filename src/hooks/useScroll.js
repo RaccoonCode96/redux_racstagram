71987a1d4febabd7a794fbcd26d213c18531a9fd
RaccoonCode96 <waawoo13@gmail.com>
2021-08-14 00:14:04 +0900

    useLayoutEffect 를 통한 umount 전 scroll 값 기억하는 hook 구현

---
+++ src/hooks/useScroll.js
@@ -0,0 +1,18 @@
+import { useLayoutEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setPrevScrollY } from '../redux/modules/post';
+
+const useScroll = () => {
+	const dispatch = useDispatch();
+	const prevScrollY = useSelector((state) => state.post.prevScrollY);
+	useLayoutEffect(() => {
+		if (prevScrollY) {
+			window.scrollTo(0, prevScrollY);
+		}
+		return () => {
+			dispatch(setPrevScrollY(window.scrollY));
+		};
+	}, []);
+};
+
+export default useScroll;
