36ba0e3a19c48abd1edd97aeb875597a3757730f
RaccoonCode96 <waawoo13@gmail.com>
2021-08-14 00:13:21 +0900

    랜덤 숫자 뽑기 (제외 번호 기입도 가능) hook 구현

--- src/hooks/useRandom.js
+++ src/hooks/useRandom.js
@@ -1,9 +1,20 @@
-const useRandom = async (count, range) => {
+const useRandom = (count, range, except) => {
 	const randomSet = new Set();
 	while (randomSet.size < count) {
-		await randomSet.add(Math.floor(Math.random() * range));
+		const temp = Math.floor(Math.random() * (range + 1));
+		if (temp === except) {
+			continue;
+		}
+		randomSet.add(temp);
 	}
 	return [...randomSet];
 };
 
 export default useRandom;
+
+/* 
+count : 뽑을 개수
+range : 정수 숫자 범위
+except : 뽑는 숫자중에 제외할 정수
+return : 일정 범위(0이상 ~ range 이하) 숫자에서 일정한 개수의 랜덤 숫자가 들어간 배열을 반환 (except 고려)
+*/
