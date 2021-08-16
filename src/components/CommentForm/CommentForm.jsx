61bef22a7f843d55f5b959ad2bf29751a8b9f2fa
RaccoonCode96 <waawoo13@gmail.com>
2021-08-16 18:22:30 +0900

    댓글 입력 컴포넌트 구현

---
+++ src/components/CommentForm/CommentForm.jsx
@@ -0,0 +1,26 @@
+import { TextField } from '@material-ui/core';
+import './CommentForm.scss';
+
+const CommentForm = ({ onChange, comment, onSubmit }) => {
+	return (
+		<form className="comment_form" onSubmit={onSubmit}>
+			<TextField
+				onChange={onChange}
+				value={comment}
+				label="댓글"
+				variant="outlined"
+				className="comment_input"
+				size="small"
+			/>
+			<button
+				type="submit"
+				className="comment_submit"
+				disabled={comment ? false : true}
+			>
+				작성
+			</button>
+		</form>
+	);
+};
+
+export default CommentForm;
