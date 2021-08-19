bbc71f9e55bda8ec9f3a00b43cef1e2c5edfc553
RaccoonCode96 <waawoo13@gmail.com>
2021-08-19 19:45:16 +0900

    무한 스크롤 기능의 컴포넌트 구현

---
+++ src/components/common/UseInfiniteScroll.jsx
@@ -0,0 +1,46 @@
+// import { CircularProgress } from '@material-ui/core';
+import { debounce } from 'lodash';
+import React, { useRef, useEffect, useMemo } from 'react';
+
+const UseInfiniteScroll = ({ execute, loading }) => {
+	const debounceExecute = useMemo(() => debounce(execute, 300), [execute]);
+	const lastElRef = useRef(null);
+	const observer = useRef(null);
+
+	useEffect(() => {
+		// 최초에 만들어지고, 나서 disconnect 하지 않고 새로 지정한다고 해도 가비지 컬렉터가 그전에 있던것을 지우지 못하는 듯함
+		if (observer.current) {
+			observer.current.disconnect();
+			console.log('observe: init');
+		}
+
+		observer.current = new IntersectionObserver(
+			([{ isIntersecting }]) => {
+				if (isIntersecting) {
+					// execute();
+					console.log('function run');
+					debounceExecute();
+				}
+			},
+			{ threshold: 0.5 }
+		);
+
+		observer.current.observe(lastElRef.current);
+		console.log('observe : is watching');
+	}, [debounceExecute]);
+
+	useEffect(() => {
+		return () => {
+			observer.current.disconnect();
+			console.log('observe : disconnected');
+		};
+	}, []);
+
+	return (
+		<div ref={lastElRef} style={{ height: '20px' }}>
+			{/* {loading && <CircularProgress />} */}
+		</div>
+	);
+};
+
+export default UseInfiniteScroll;
