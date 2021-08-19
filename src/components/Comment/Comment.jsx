51090d921099ab3eed9a2788fe7742720bafd2fd
RaccoonCode96 <waawoo13@gmail.com>
2021-08-19 19:43:16 +0900

    댓글 요소안에 댓글 작성자 이름이 포함되는 관계로 변경

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -25,10 +25,12 @@ const Comment = ({ commentObj, postId }) => {
 		<li className="comment_container" key={commentId}>
 			<img src={userPhotoUrl} alt="user_image" className="user_image" />
 			<div className="comment_info">
-				<Link to={`/user/${userDisplayName}`} className="comment_user_name">
-					{userDisplayName}
-				</Link>
-				<span className="comment">{comment}</span>
+				<span className="comment">
+					<Link to={`/user/${userDisplayName}`} className="comment_user_name">
+						{userDisplayName}
+					</Link>
+					{comment}
+				</span>
 			</div>
 			{currentUserId === userId && (
 				<button onClick={confirmToggle} className="delete_comment_btn">
