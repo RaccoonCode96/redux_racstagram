badf57a05e427388dc0ee324b49d2e38496bf932
RaccoonCode96 <waawoo13@gmail.com>
2021-08-31 16:31:59 +0900

    currentUser, User 통합으로 인해 기존의 pathname 등의 분리 조건 삭제 및 리팩토링

--- src/components/common/ProfilePostImages.jsx
+++ src/components/common/ProfilePostImages.jsx
@@ -1,19 +1,13 @@
-import { useCallback, useLayoutEffect } from 'react';
+import { useCallback, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useHistory, useLocation, useParams } from 'react-router-dom';
-import {
-	getMorePostsThunk,
-	resetGetCurrentUserPosts,
-	resetGetMorePosts,
-	resetGetUserPosts,
-} from '../../redux/modules/post';
+import { useHistory, useParams } from 'react-router-dom';
+import { getMorePostsThunk, resetGetMorePosts } from '../../redux/modules/post';
 import './ProfilePostImages.scss';
 import UseInfiniteScroll from './UseInfiniteScroll';
 
 const ProfilePostImages = ({ posts }) => {
 	// props : posts -> 현재 유저 인지 특정 유저인지에 따라 들어오는 posts가 다름
 	const { userName } = useParams();
-	const { pathname } = useLocation();
 	const history = useHistory();
 	const dispatch = useDispatch();
 
@@ -24,36 +18,21 @@ const ProfilePostImages = ({ posts }) => {
 	// 특정 유저 Posts 가져오기 성공 상태
 	const isGetUserPosts = useSelector((state) => state.post.getUserPosts.isGet);
 
-	// 현재 유저 Posts 가져오기 성공 상태
-	const isGetCurrentUserPosts = useSelector(
-		(state) => state.post.getCurrentUserPosts.isGet
-	);
-
 	// 무한 스크롤시 더 가져오기 요청하는 함수 (현재 유저인지, 특정 유저인지 구분)
 	const getMorePosts = useCallback(() => {
 		const postDate = posts[posts.length - 1]?.postDate;
-		if (pathname === '/profile') {
-			dispatch(getMorePostsThunk({ postDate, type: 'currentUserPosts' }));
-		} else if (pathname === `/user/${userName}`) {
-			dispatch(getMorePostsThunk({ postDate, type: 'userPosts', userName }));
-		}
-	}, [dispatch, pathname, posts, userName]);
+		dispatch(getMorePostsThunk({ postDate, type: 'userPosts', userName }));
+	}, [dispatch, posts, userName]);
 
 	// 유저의 작성글 이미지 클릭시 해당 글로 이동 요청(클릭한 요소가 몇번째 인지를 같이 보냄)
 	const toPosts = useCallback(
 		(postNum) => {
-			if (pathname === '/profile') {
-				history.push({ pathname: '/profile/posts', state: { postNum } });
-			} else if (pathname === `/user/${userName}`) {
-				history.push({
-					pathname: `/user/${userName}/posts`,
-					state: { postNum },
-				});
-			} else {
-				console.log('invalid location request');
-			}
+			history.push({
+				pathname: `/user/${userName}/posts`,
+				state: { postNum },
+			});
 		},
-		[history, userName, pathname]
+		[history, userName]
 	);
 
 	// 들어온 posts를 imagesTable(3열)로 구성하기 위한 posts를 나누는 함수
@@ -72,11 +51,10 @@ const ProfilePostImages = ({ posts }) => {
   - getMorePosts의 isNone 상태 reset
   - 과거 Posts 데이터 화면 표시 현상 제거를 위해 항상 unmount시 가져왔던 데이터는 reset 요청
   */
-	useLayoutEffect(() => {
-		return async () => {
+	useEffect(() => {
+		return () => {
 			dispatch(resetGetMorePosts());
-			dispatch(resetGetUserPosts());
-			dispatch(resetGetCurrentUserPosts());
+			// dispatch(resetGetUserPosts());
 		};
 	}, [dispatch]);
 
@@ -107,7 +85,7 @@ const ProfilePostImages = ({ posts }) => {
 					</div>
 				))}
 			</div>
-			{!isNone && (isGetCurrentUserPosts || isGetUserPosts) && (
+			{!isNone && isGetUserPosts && (
 				<UseInfiniteScroll execute={getMorePosts} />
 			)}
 		</>
